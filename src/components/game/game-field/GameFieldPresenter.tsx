4ba30e2394f3284775170e238dc4850113a9d28f
Ondřej Mastík <ondramastik@outlook.com>
2021-10-21 20:09:36 +0200

    Added styled components and used them in GameFieldPresenter

--- src/components/game/game-field/GameFieldPresenter.tsx
+++ src/components/game/game-field/GameFieldPresenter.tsx
@@ -1,17 +1,43 @@
 import React, {FC} from 'react';
-import './GameFieldPresenter.css';
 import GameView from "../../../domain/GameView";
 import Coordinates from "../../../domain/Coordinates";
 import {TileType} from "../../../domain/TileType";
+import styled from "styled-components";
 
 interface Props {
   gameView: GameView
 }
 
+const Container = styled.div`
+  width: 100%;
+  line-height: 7px;
+`
+const Tile = styled.div`
+  width: 14px;
+  height: 14px;
+  display: inline-block;
+`
+
+const FloorTile = styled(Tile)`
+  background-color: lightgray;
+`
+
+const WallTile = styled(Tile)`
+  background-color: #BC4A3C;
+`
+
+const SnakeTile = styled(Tile)`
+  background-color: black;
+`
+
+const FoodTile = styled(Tile)`
+  background-color: #4dbc3c;
+`
+
 const resolveTile = (gameField: GameView, pos: Coordinates) => {
-  if(gameField.snakeTiles.filter(location => location.X === pos.X && location.Y === pos.Y).length > 0) {
+  if (gameField.snakeTiles.filter(location => location.X === pos.X && location.Y === pos.Y).length > 0) {
     return TileType.Snake
-  } else if(gameField.foodLocation.X === pos.X && gameField.foodLocation.Y === pos.Y) {
+  } else if (gameField.foodLocation.X === pos.X && gameField.foodLocation.Y === pos.Y) {
     return TileType.Food
   }
 
@@ -19,14 +45,27 @@ const resolveTile = (gameField: GameView, pos: Coordinates) => {
 }
 
 const GameFieldPresenter: FC<Props> = ({gameView}) => {
-  return <div id="game-field-presenter">
+  const getTile = (gameView: GameView, pos: Coordinates) => {
+    const resolvedTile = resolveTile(gameView, pos)
+
+    switch (resolvedTile) {
+      case TileType.Wall:
+        return <WallTile/>
+      case TileType.Snake:
+        return <SnakeTile/>
+      case TileType.Food:
+        return <FoodTile/>
+    }
+
+    return <FloorTile/>
+  }
+
+  return <Container id="game-field-presenter">
     {gameView.tiles.map(
-      (row, x) => <React.Fragment>
-        {row.map((tile, y) => <div
-          className={`tile tile-${resolveTile(gameView, {X: x, Y: y})}`} key={`${x}-${y}`}/>)}
-        <div/>
-      </React.Fragment>)}
-  </div>
+      (row, x) => <div>
+        {row.map((tile, y) => getTile(gameView, {X: x, Y: y}))}
+      </div>)}
+  </Container>
 }
 
 export default GameFieldPresenter
